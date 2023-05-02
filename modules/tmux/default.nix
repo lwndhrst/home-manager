bb4a7ac32dbf57dfd5d30622f4a87fe68ce77acb
Leon Windhorst <leon@windhorst.dev>
2023-05-02 14:06:45 +0200

    chore: remove vi keymode

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -5,7 +5,6 @@
     enable = true;
     mouse = true;
     escapeTime = 10;
-    keyMode = "vi";
     terminal = "screen-256color";
     extraConfig = ''
       # required for neovim to display correct colors inside tmux
