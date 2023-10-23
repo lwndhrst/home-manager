dbf932c5bf66f2cc6990c780bd88efadab87fa7b
Leon Windhorst <leon@windhorst.dev>
2023-10-23 10:11:26 +0200

    refactor: maybe fix flake output?

--- hosts/default.nix
+++ hosts/default.nix
@@ -19,7 +19,15 @@ let
   };
 
 in {
-  desktop = nixosModule ./desktop/home.nix;
-  laptop = nixosModule ./laptop/home.nix;
-  vbox = nixosModule ./vbox/home.nix;
+  desktop = {
+    config = nixosModule ./desktop/home.nix;
+  };
+
+  laptop = {
+    config = nixosModule ./laptop/home.nix;
+  };
+
+  vbox = {
+    config = nixosModule ./vbox/home.nix;
+  };
 }
