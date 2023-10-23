56ec92cdf418c360e5afc2fc431968c921b8c7e5
Leon Windhorst <leon@windhorst.dev>
2023-10-23 07:52:52 +0200

    chore: add fzf scripts for cd and editing

--- modules/fzf/default.nix
+++ modules/fzf/default.nix
@@ -1,25 +1,46 @@
 { pkgs, ... }:
 
-{
+let
+  fzf-cd = pkgs.writeShellScriptBin "fcd" ''
+    #!${pkgs.bash}/bin/bash
+    DIR=$(${pkgs.fd}/bin/fd --type directory | ${pkgs.fzf}/bin/fzf)
+    if [[ $DIR && -d $DIR ]]; then
+      cd $DIR
+    fi
+  '';
+  fzf-edit = pkgs.writeShellScriptBin "fe" ''
+    #!${pkgs.bash}/bin/bash
+    FILE=$(${pkgs.fd}/bin/fd --type file | ${pkgs.fzf}/bin/fzf)
+    if [[ $FILE && -f $FILE ]]; then
+      $EDITOR $FILE
+    fi
+  '';
+
+in {
+  home.packages = [
+    fzf-cd   # fcd
+    fzf-edit # fe
+  ];
+
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
     colors = { 
-      "fg" = "#908caa";
-      "bg" = "#0c0b11";
-      "hl" = "#ebbcba";
-      "fg+" = "#e0def4";
-      "bg+" = "#26233a";
-      "hl+" = "#ebbcba";
-      "border" = "#403d52";
-      "header" = "#31748f";
-      "gutter" = "#0c0b11";
-      "spinner" = "#f6c177";
-      "info" = "#9ccfd8";
+      "fg"        = "#908caa";
+      "bg"        = "#0c0b11";
+      "hl"        = "#ebbcba";
+      "fg+"       = "#e0def4";
+      "bg+"       = "#26233a";
+      "hl+"       = "#ebbcba";
+      "border"    = "#403d52";
+      "header"    = "#31748f";
+      "gutter"    = "#0c0b11";
+      "spinner"   = "#f6c177";
+      "info"      = "#9ccfd8";
       "separator" = "#403d52";
-      "pointer" = "#c4a7e7";
-      "marker" = "#eb6f92";
-      "prompt" = "#908caa";
+      "pointer"   = "#c4a7e7";
+      "marker"    = "#eb6f92";
+      "prompt"    = "#908caa";
     };
   };
 }
